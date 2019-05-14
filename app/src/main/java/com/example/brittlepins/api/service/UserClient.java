d000a413cf61b1218e9944c0ceb80217a0506ba4
ekaterina-nikonova <ekaterina.v.nikonova@gmail.com>
2019-05-14 19:28:03 +0200

    Set up Retrofit in main activity, UserClient interface, base URLs for different build types

--- app/src/main/java/com/example/brittlepins/api/service/UserClient.java
+++ app/src/main/java/com/example/brittlepins/api/service/UserClient.java
@@ -1,4 +1,20 @@
 package com.example.brittlepins.api.service;
 
-public class UserClient {
+import com.example.brittlepins.api.model.Login;
+import com.example.brittlepins.api.model.User;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.POST;
+
+public interface UserClient {
+    @POST("signin")
+    Call<User> login(@Body Login login);
+
+    @GET("boards")
+    Call<ResponseBody> getBoards(@Header("X-CSRF-TOKEN") String authToken);
+
 }
--- app/src/main/java/com/example/brittlepins/ui/MainActivity.java
+++ app/src/main/java/com/example/brittlepins/ui/MainActivity.java
@@ -4,9 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.brittlepins.BuildConfig;
 import com.example.brittlepins.R;
+import com.example.brittlepins.api.service.UserClient;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
+    Retrofit.Builder builder = new Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create());
+    Retrofit retrofit = builder.build();
+    UserClient userClient = retrofit.create(UserClient.class);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
